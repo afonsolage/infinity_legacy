5b27e386304a9ed503d0e98fae9b1afee00f22ed
Afonso Lage <lage.afonso@gmail.com>
2019-01-29 08:39:34 -0300

    Initial commit

---
+++ core/src/main/kotlin/com/lagecompany/infinity/InfinityGame.kt
@@ -0,0 +1,9 @@
+package com.lagecompany.infinity
+
+import com.jme3.app.SimpleApplication
+
+class InfinityGame : SimpleApplication() {
+    override fun simpleInitApp() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ desktop/src/main/kotlin/com/lagecompany/infinity/desktop/main.kt
@@ -0,0 +1,8 @@
+package com.lagecompany.infinity.desktop
+
+import com.lagecompany.infinity.InfinityGame
+
+fun main(args: Array<String>) {
+    val game = InfinityGame()
+    game.start()
+}
